516980f5aad1d9f6f71f56f8aceffbbb28bf7f43
qwreey75 <qwreey75@gmail.com>
2021-05-09 00:23:44 +0900

    'init'

---
+++ info.lua
@@ -0,0 +1,16 @@
+return {
+    moduleName = "Lib-MaterialUI";
+    moduleAuthor = "qwreey75";
+    import = {
+    };
+    github = "https://github.com/nofairTCM/Lib-MaterialUI";
+    lic = "MIT";
+    info = [[
+        https://github.com/qwreey75/RBX_UI_PROJECT/tree/master/MaterialUI 으로 부터 분기된 모듈입니다, 일부 수정사항을 담고 있을 수 있으며 일부 모듈이 필요로 할 때 사용됩니다
+    ]];
+    publishStatus = "NONE";
+    version = "1";
+    buildVersion = 1;
+    pluginIgnore = false;
+    type = "lib";
+};
